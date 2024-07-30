f5a8014877eba7d558a7afa5ca580e2fe0b4f904
sashkashhh <chenneldps@gmail.com>
2024-07-31 00:06:33 +0300

    добавил getEventsInArea

--- src/controllers/eventController.js
+++ src/controllers/eventController.js
@@ -1,5 +1,26 @@
 const Event = require('../models/eventModel');
 
+const getDistance = (pointA, pointB) => {
+  const latA = Number(pointA[1]) * Math.PI / 180
+  const longA = Number(pointA[0]) * Math.PI / 180
+  const latB = Number(pointB[1]) * Math.PI / 180
+  const longB = Number(pointB[0]) * Math.PI / 180
+
+  const cl1 = Math.cos(latA);
+  const cl2 = Math.cos(latB);
+  const sl1 = Math.sin(latA);
+  const sl2 = Math.sin(latB);
+  const delta = longA - longB;
+  const cdelta = Math.cos(delta);
+  const sdelta = Math.sin(delta);
+
+  const y = Math.sqrt(Math.pow(cl2 * sdelta, 2) + Math.pow(cl1 * sl2 - sl1 * cl2 * cdelta, 2));
+  const x = sl1 * sl2 + cl1 * cl2 * cdelta;
+  const distance = Math.atan2(y, x) * 6371;
+  
+  return distance
+}
+
 const createEvent = async (req, res) => {
     const { type, description, coordinates, address, userId } = req.body;
 
@@ -28,4 +49,15 @@ const getAllEvents = async (req, res) => {
   }
 };
 
-module.exports = { createEvent, getAllEvents };
+const getEventsInArea = async (req, res) => {
+  const { searchCenter, searchRadius } = req.query
+  try {
+    const events = await Event.find()
+    const filteredEvents = events.filter((event) => getDistance(event.coordinates, searchCenter) < searchRadius)
+    res.status(200).json(filteredEvents)
+  } catch (error) {
+    res.status(500).json({ message: error.message })
+  }
+}
+
+module.exports = { createEvent, getAllEvents, getEventsInArea };
--- src/routes/eventRouter.js
+++ src/routes/eventRouter.js
@@ -1,8 +1,8 @@
 const express = require('express');
-const { createEvent, getAllEvents } = require('../controllers/eventController');
+const { createEvent, getAllEvents, getEventsInArea } = require('../controllers/eventController');
 const router = express.Router();
 
 router.post('/events', createEvent);
-router.get('/events', getAllEvents)
-
+router.get('/events', getAllEvents);
+router.get('/events/area', getEventsInArea);
 module.exports = router;

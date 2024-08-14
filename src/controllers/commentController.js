8fca651aed0d3c6844a787f99abd477dc800907d
sashkashhh <chenneldps@gmail.com>
2024-08-14 16:32:24 +0300

    Убрал загрузку видео

--- src/controllers/commentController.js
+++ src/controllers/commentController.js
@@ -20,14 +20,14 @@ storage: storage,
 limits: { fileSize: 10 * 1024 * 1024 }, 
 fileFilter: function (req, file, cb) {
 
-    const filetypes = /jpeg|jpg|png|gif|mp4|mov|avi/;
+    const filetypes = /jpeg|jpg|png/;
     const mimetype = filetypes.test(file.mimetype);
     const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
 
     if (mimetype && extname) {
         return cb(null, true);
     } else {
-        cb(new Error('Only images and videos are allowed!'));
+        cb(new Error('Only images are allowed!'));
     }
 }
 });
